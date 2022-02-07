5e24367a73c59ca5d659999dcddc8dcc6897a52f
qlemql <taehyun_fe@naver.com>
2022-02-07 20:22:19 +0900

    Tutorial
    설치한 플러그인 내용 추가

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,17 @@
 module.exports = {
   siteMetadata: {
-      title: `기록저장소`,
-    siteUrl: `https://www.yourdomain.tld`
+    title: `기록저장소`,
+    siteUrl: `https://www.yourdomain.tld`,
   },
-  plugins: ["gatsby-plugin-netlify-cms"]
+  plugins: [
+    "gatsby-plugin-netlify-cms",
+    {
+      resolve: `gatsby-plugin-typescript`,
+      options: {
+        isTSX: true, // defaults to false
+        jsxPragma: `jsx`, // defaults to "React"
+        allExtensions: true, // defaults to false
+      },
+    },
+  ],
 };
