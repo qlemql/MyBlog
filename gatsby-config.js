d83072c9fcefb5e18d51d6db33a2bb875cc5b49b
qlemql <taehyun_fe@naver.com>
2022-02-11 22:18:51 +0900

    Tutorial End

--- gatsby-config.js
+++ gatsby-config.js
@@ -21,5 +21,22 @@ module.exports = {
       },
     },
     "gatsby-plugin-mdx",
+    "gatsby-transformer-sharp",
+    {
+      resolve: `gatsby-plugin-sharp`,
+      options: {
+        // Defaults used for gatsbyImageData and StaticImage
+        defaults: {},
+        // Set to false to allow builds to continue on image errors
+        failOnError: true,
+        // deprecated options and their defaults:
+        base64Width: 20,
+        forceBase64Format: `png`, // valid formats: png,jpg,webp
+        useMozJpeg: process.env.GATSBY_JPEG_ENCODER === `MOZJPEG`,
+        stripMetadata: true,
+        defaultQuality: 50,
+      },
+    },
+    "gatsby-plugin-image",
   ],
 };
